c8422c2ab0d9a0831541543dcc9c94643d0ce5db
m-sef <seth.l.moore0506@gmail.com>
2024-08-30 13:42:37 -0400

    Contemplating style...

--- src/main.c
+++ src/main.c
@@ -27,7 +27,9 @@ static void display_help()
 	puts("  B3/S23               Life");
 	puts("  B35678/S5678         Diamoeba");
 	puts("  B4678/S35678         Anneal");
-	puts("  B3/S12345             Maze");
+	puts("  B3/S12345            Maze");
+	puts("  B25678/S5678         Iceballs");
+	puts("  B3/S45678            Coral");
 }
 
 static void display_version()
--- src/ruleset.h
+++ src/ruleset.h
@@ -15,10 +15,12 @@ typedef struct
 
 RULESET* init_ruleset(char* rulestring);
 void parse_rulestring(RULESET* ruleset, char* rulestring);
+
 void set_birth_flag(RULESET* ruleset, const uint8_t n);
 bool get_birth_flag(const RULESET* ruleset, const uint8_t n);
 void set_survive_flag(RULESET* ruleset, const uint8_t n);
 bool get_survive_flag(const RULESET* ruleset, const uint8_t n);
+
 void reverse(RULESET* ruleset);
 static uint16_t reverse_bits(uint16_t val);
 
